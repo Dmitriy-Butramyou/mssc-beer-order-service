d99ea2bb02ca6348d11688c5ad0f0a742e9d9ab5
D.Butramyou <d.butramyou@gmail.com>
2021-06-04 23:23:19 +0300

    Validate Order Response

--- src/main/java/home/sfg/beer/order/service/services/BeerOrderManager.java
+++ src/main/java/home/sfg/beer/order/service/services/BeerOrderManager.java
@@ -2,7 +2,11 @@ package home.sfg.beer.order.service.services;
 
 import home.sfg.beer.order.service.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
   BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+  void processValidationResult(UUID beerOrderId, Boolean isValid);
 }
