387d4b6c0575e76537daa7aa88b71836228ad719
D.Butramyou <d.butramyou@gmail.com>
2021-06-04 15:27:57 +0300

    Validate Order Request

--- src/main/java/home/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/home/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -2,7 +2,9 @@ package home.sfg.beer.order.service.sm;
 
 import home.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import home.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
@@ -10,9 +12,12 @@ import org.springframework.statemachine.config.builders.StateMachineTransitionCo
 
 import java.util.EnumSet;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableStateMachineFactory
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+  private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validationOrderAction;
 
   @Override
   public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -30,6 +35,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     transitions.withExternal()
             .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
             .event(BeerOrderEventEnum.VALIDATE_ORDER)
+            .action(validationOrderAction)
         .and().withExternal()
             .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
             .event(BeerOrderEventEnum.VALIDATION_PASSED)
